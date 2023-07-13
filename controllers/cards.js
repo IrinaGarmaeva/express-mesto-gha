093f12fe6ae638a6a8689f23d39765303e5e476c
Irina Garmaeva <garmaeva-i@mail.ru>
2023-07-13 15:57:22 +0700

    feat: add error codes, refactore: controllers

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,9 +1,14 @@
 const Card = require('../models/card');
+const {
+  ERROR_BAD_REQUEST,
+  ERROR_NOT_FOUND,
+  ERROR_INTERNAL_SERVER,
+} = require('../errors/errors');
 
 async function getCards(req, res) {
   return Card.find({})
-    .then((cards) => res.status(200).send({ cards }))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .then((cards) => res.send({ cards }))
+    .catch(() => res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' }));
 }
 
 function createCard(req, res) {
@@ -14,10 +19,10 @@ function createCard(req, res) {
     .then((card) => res.status(201).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании карточки.' });
         return;
       }
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' });
     });
 }
 
@@ -27,17 +32,17 @@ function deleteCard(req, res) {
   return Card.findByIdAndRemove(cardId)
     .then((card) => {
       if (!card) {
-        res.status(404).send({ message: `Карточка с указанным _id: ${cardId} не найдена.` });
+        res.status(ERROR_NOT_FOUND).send({ message: `Карточка с указанным _id: ${cardId} не найдена.` });
         return;
       }
       res.send(card);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при удалении карточки.' });
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при удалении карточки.' });
         return;
       }
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' });
     });
 }
 
@@ -45,17 +50,17 @@ function addLike(req, res) {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .then((card) => {
       if (card === null) {
-        res.status(404).send({ message: `Передан несуществующий _id: ${req.params.cardId} карточки.` });
+        res.status(ERROR_NOT_FOUND).send({ message: `Передан несуществующий _id: ${req.params.cardId} карточки.` });
         return;
       }
-      res.status(200).send(card);
+      res.send(card);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка.' });
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные для постановки лайка.' });
         return;
       }
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' });
     });
 }
 
@@ -63,17 +68,17 @@ function removeLike(req, res) {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then((card) => {
       if (card === null) {
-        res.status(404).send({ message: `Передан несуществующий _id: ${req.params.cardId} карточки.` });
+        res.status(ERROR_NOT_FOUND).send({ message: `Передан несуществующий _id: ${req.params.cardId} карточки.` });
         return;
       }
-      res.status(200).send(card);
+      res.send(card);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные для снятия лайка.' });
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные для снятия лайка.' });
         return;
       }
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' });
     });
 }
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,14 @@
 const User = require('../models/user');
+const {
+  ERROR_BAD_REQUEST,
+  ERROR_NOT_FOUND,
+  ERROR_INTERNAL_SERVER,
+} = require('../errors/errors');
 
 function getUsers(req, res) {
   return User.find({})
-    .then((users) => res.status(200).send(users))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .then((users) => res.send(users))
+    .catch(() => res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' }));
 }
 
 function getUser(req, res) {
@@ -12,17 +17,17 @@ function getUser(req, res) {
   return User.findById(userId)
     .then((user) => {
       if (!user) {
-        res.status(404).send({ message: `Пользователь по указанному _id: ${userId} не найден` });
+        res.status(ERROR_NOT_FOUND).send({ message: `Пользователь по указанному _id: ${userId} не найден` });
         return;
       }
-      res.status(200).send(user);
+      res.send(user);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при запросе пользователя.' });
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при запросе пользователя.' });
         return;
       }
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' });
     });
 }
 
@@ -33,54 +38,51 @@ function createUser(req, res) {
     .then((user) => res.status(201).send({ user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании пользователя.' });
         return;
       }
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' });
     });
 }
 
 function updateUserInfo(req, res) {
   const { name, about } = req.body;
 
-  return User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+  return User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
-        res.status(404).send({ message: `Пользователь с указанным _id: ${req.user._id} не найден.` });
+        res.status(ERROR_NOT_FOUND).send({ message: `Пользователь с указанным _id: ${req.user._id} не найден.` });
         return;
       }
-      if ((name.length < 2 || name.length > 30) || (about.length < 2 || about.length > 30)) {
-        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
-        return;
-      }
-      res.status(200).send(user);
+      res.send(user);
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+      console.log(err.name);
+      if (err.name === 'ValidationError' || 'CastError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
         return;
       }
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' });
     });
 }
 
 function updateAvatar(req, res) {
   const { avatar } = req.body;
 
-  return User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+  return User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
-        res.status(404).send({ message: `Пользователь с указанным _id: ${req.user._id} не найден.` });
+        res.status(ERROR_NOT_FOUND).send({ message: `Пользователь с указанным _id: ${req.user._id} не найден.` });
         return;
       }
-      res.status(200).send(user);
+      res.send(user);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
         return;
       }
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER).send({ message: 'На сервере произошла ошибка' });
     });
 }
 
